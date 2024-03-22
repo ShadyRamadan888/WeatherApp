00f5f051d5b0332a7d25b32d308657a0403609b6
Shady Ramadan <89228710+ShadyRamadan888@users.noreply.github.com>
2024-03-22 23:02:03 +0200

    feat: seven day forecast use case in domain layer

---
+++ domain/src/main/java/com/weather/domain/usecases/seven_days_forecast_usecase/SevenDaysForecastUseCase.kt
@@ -0,0 +1,17 @@
+package com.weather.domain.usecases.seven_days_forecast_usecase
+
+import com.weather.data.local.SharedPreferenceManager
+import com.weather.data.model.WeatherList
+import com.weather.data.repo.WeatherRepo
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class SevenDaysForecastUseCase @Inject constructor(
+    private val sharedPreferenceManager: SharedPreferenceManager,
+    private val repo: WeatherRepo
+) : ISevenDaysForecastUseCase {
+    override fun invoke(): Flow<List<WeatherList>> {
+        val city = sharedPreferenceManager.getCityName()
+        return repo.get7DayForecast(city)
+    }
+}
